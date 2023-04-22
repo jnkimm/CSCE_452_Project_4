7669f13e4c9e79067b8999134ad83f01bbd191f6
EvelynDRose <54924148+EvelynDRose@users.noreply.github.com>
2023-04-21 21:19:21 -0500

    new and improved launch file

--- launch/launch.py
+++ launch/launch.py
@@ -18,67 +18,69 @@ def disc_robot_urdf(robot):
     height = robot['body']['height']
 
     return (f"""<?xml version="1.0"?>
-<robot name="disc">
-    <material name="light_blue">
-        <color rgba="0.5 0.5 1 1"/>
-    </material>
-    <material name="dark_blue">
-        <color rgba="0.1 0.1 1 1"/>
-    </material>
-    <material name="dark_red">
-        <color rgba="1 0.1 0.1 1"/>
-    </material>
-    <link name="base_link">
-        <visual>
-            <geometry>
-                <cylinder length="{height}" radius="{radius}"/>
-            </geometry>
-            <material name="light_blue"/>
-        </visual>
-    </link>
-    <link name="heading_box">
-        <visual>
-            <geometry>
-                <box size="{0.9*radius} {0.2*radius} {1.2*height}"/>
-            </geometry>
-            <material name="dark_blue"/>
-        </visual>
-    </link>
-    <link name="laser" />
-    <joint name="base_to_heading_box" type="fixed">
-        <parent link="base_link"/>
-        <child link="heading_box"/>
-        <origin xyz='{0.45*radius} 0.0 0.0'/>
-    </joint>
-    <joint name="base_to_laser" type="fixed">
-        <parent link="base_link"/>
-        <child link="laser"/>
-        <origin xyz="{0.5*radius} 0.0 0.0"/>
-    </joint>
-</robot>
-""")
+    <robot name="disc">
+        <material name="light_blue">
+            <color rgba="0.5 0.5 1 1"/>
+        </material>
+        <material name="dark_blue">
+            <color rgba="0.1 0.1 1 1"/>
+        </material>
+        <material name="dark_red">
+            <color rgba="1 0.1 0.1 1"/>
+        </material>
+        <link name="base_link">
+            <visual>
+                <geometry>
+                    <cylinder length="{height}" radius="{radius}"/>
+                </geometry>
+                <material name="light_blue"/>
+            </visual>
+        </link>
+        <link name="heading_box">
+            <visual>
+                <geometry>
+                    <box size='{0.9*radius} {0.2*radius} {1.2*height}'/>
+                </geometry>
+                <material name="dark_blue"/>
+            </visual>
+        </link>
+        <link name="laser" />
+        <joint name="base_to_heading_box" type="fixed">
+            <parent link="base_link"/>
+            <child link="heading_box"/>
+            <origin xyz='{1.45*radius} 0.0 0.0'/>
+        </joint>
+        <joint name="base_to_laser" type="fixed">
+            <parent link="base_link"/>
+            <child link="laser"/>
+            <origin xyz='{1.5*radius} 0.0 0.0'/>
+        </joint>
+    </robot>
+    """)
 
 def generate_launch_description():
 
     # Arguments
-    map_arg = DeclareLaunchArgument('map')
-    robot_arg = DeclareLaunchArgument('robot')
-    ld = LaunchDescription([robot_arg])
-    robot = LaunchConfiguration('robot')
-    setup_map_node = Node(
-        package = "project_4", 
-        executable = "setup_map"
-        )
+    bag_arg = DeclareLaunchArgument('bag_in')
+    ld = LaunchDescription([bag_arg])
+
+    # Nodes
     robot_node = Node(
         package = "robot_state_publisher", 
         executable = "robot_state_publisher", 
         name= "robot_state_publisher", 
         output="screen", 
-        parameters=[{"robot_description": load_robot("normal.robot")['urdf']}]
+        parameters=[{"robot_description": load_robot("normal.robot")['urdf']}] # robot description
         )
 
     # starting node
     ld.add_action(robot_node)
-    ld.add_action(setup_map_node)
+    setup_map = ExecuteProcess(cmd = ['python3', 'project_4/setup_map.py'])
+    ld.add_action(setup_map)
+    
+    # bag 
+    bag = LaunchConfiguration('bag_in')
+    ep = ExecuteProcess(cmd = ['ros2', 'bag', 'play', bag])
+    ld.add_action(ep)
 
     return ld
